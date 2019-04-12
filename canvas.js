df678a05c4c2896089eb09e5af5e98799542b205
Unknown <rispir45@gmail.com>
2019-04-12 01:42:50 -0400

    Implemented canvas functionality
    close #1

--- canvas.js
+++ canvas.js
@@ -1,2 +1,20 @@
-// This module will provide functionality to alter our canvas object
-canvasObject = document.getElementById("canvas").setAttribute("fill", "black");
+// This script provides functionality to change the canvas dimensions
+
+// Grab our form object and add an event listener to it
+const form = document.querySelector('form#canvasForm')
+form.addEventListener('submit', (ev) => {
+  ev.preventDefault()
+  handleSubmit(ev)
+})
+
+function changeCanvasSize(width, height) {
+  const canvas = document.getElementById('canvas')
+  canvas.setAttribute('width', width)
+  canvas.setAttribute('height', height)
+}
+
+function handleSubmit(ev) {
+  const c = ev.target
+  changeCanvasSize(c.widthInput.value, c.heightInput.value)
+}
+
